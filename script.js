62a12e95b1d284b6bf4eb62b232f341e8a385620
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-05-17 15:41:49 -0500

    preview box interactive with images

--- script.js
+++ script.js
@@ -19,4 +19,21 @@ window.onload = () => {
       })
     }
   }
+  for (let index = 0; index < filterImg.length; index++) {
+    filterImg[index].setAttribute("onclick", "preview(this)");
+  }
 }
+
+const previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon");
+
+function preview(element) {
+  let selectedPrevImg = element.querySelector("img").src;
+  previewImg.src = selectedPrevImg;
+  previewBox.classList.add("show");
+  closeIcon.onclick = () => {
+    previewBox.classList.remove("show");
+  }
+}
+
